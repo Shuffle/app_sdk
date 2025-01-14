724eae9a2415b21a2b1a96bf68f57c72ad8c09b0
Frikky <frikky@shuffler.io>
2025-01-14 14:16:25 +0100

    New sdk release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shuffle_sdk',  
-    version='0.0.4',  
+    version='0.0.5',  
     description='The SDK used for Shuffle',  
     py_modules=["shuffle_sdk"],  
     license='MIT',
