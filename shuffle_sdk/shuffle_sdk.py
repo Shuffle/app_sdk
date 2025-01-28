419eaef33c088cd877f5f8c9ad7d4d107fbe1532
yashsinghcodes <yash9vardhan@gmail.com>
2025-01-28 20:28:18 +0530

    [fixed]: Logical error in shuffle_default

--- shuffle_sdk/shuffle_sdk.py
+++ shuffle_sdk/shuffle_sdk.py
@@ -112,8 +112,10 @@ def base64_encode(a):
 @shuffle_filters.register
 def shuffle_default(a, default_value):
     if not a:
-        if str(a).strip().lower() in ["none", "nil", "null", "undefined", ""]:
-            return default_value
+        return default_value
+
+    if str(a).strip().lower() in ["none", "nil", "null", "undefined", ""]:
+        return default_value
 
     return a
 
