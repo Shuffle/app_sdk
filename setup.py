7014b10e758198b618a9e52560ff315ddd059fef
Frikky <frikky@shuffler.io>
2025-02-10 10:28:58 +0100

    Bumped shuffle sdk

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shuffle_sdk',  
-    version='0.0.10',  
+    version='0.0.11',  
     description='The SDK used for Shuffle',  
     py_modules=["shuffle_sdk"],  
     license='MIT',
