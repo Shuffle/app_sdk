4794fc38dfc3f1fd341de5d32cf6d7c7cd2c5b51
Frikky <frikky@shuffler.io>
2025-03-13 14:18:41 +0100

    csv_parse export issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shuffle_sdk',  
-    version='0.0.15',  
+    version='0.0.16',  
     description='The SDK used for Shuffle',  
     py_modules=["shuffle_sdk"],  
     license='MIT',
