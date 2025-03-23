90402f229b9993f9728aaff3e7dc72cb657ee34d
Frikky <frikky@shuffler.io>
2025-03-24 00:33:20 +0100

    Bump with using 0.0.9 of shufflepy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shuffle_sdk',  
-    version='0.0.19',  
+    version='0.0.20',  
     description='The SDK used for Shuffle',  
     py_modules=["shuffle_sdk"],  
     license='MIT',
